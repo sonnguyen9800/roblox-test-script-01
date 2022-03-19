e872ee8624ccae9dbb5eca896984903f65b8c7e9
Son Nguyen Hoang <sonnguyen9800@gmail.com>
2022-03-19 11:54:32 +0700

    Test ModuleScript

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -1,1 +1,6 @@
+local replicatedStorage = game:GetService("ReplicatedStorage")
+--local npcModule = require(replicatedStorage.Common.npc.npcModule);
+local npcModule = require(game.ReplicatedStorage.Common.npc);
+npcModule.initialize()
+
 print("Hello world, from client!")
---
+++ src/shared/npc.lua
@@ -0,0 +1,8 @@
+--module NPC
+local npcModule = {}
+npcModule.initialize = function()
+    print("Called from NPC");
+
+end
+
+return npcModule;
